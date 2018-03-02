8b1bfcbbb077ee0fa2ee1751ce71c5d9f76b492c
ab <underr.ua@gmail.com>
2018-03-02 14:30:27 +0200

    z

--- bin/makebin.sh
+++ bin/makebin.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-rm total-order-discount.zip
+# rm old zip
+rm -f breadcrumbs-seo.zip
+
 cp ../LICENSE ./
 cp ../README.md ./
 
-# make total-order-discount.ocmod.zip
+# make breadcrumbs-seo-microdata.ocmod.zip and breadcrumbs-seo-rdfa.ocmod.zip
+
 cd ../src/
-zip -9 -r total-order-discount.ocmod.zip *
-mv total-order-discount.ocmod.zip ../bin/
+cp install-microdata.xml install.xml
+zip -9 -r breadcrumbs-seo-microdata.ocmod.zip install.xml
+rm -f install.xml
+cp install-rdfa.xml install.xml
+zip -9 -r breadcrumbs-seo-rdfa.ocmod.zip install.xml
+rm -f install.xml
+
+mv breadcrumbs-seo-microdata.ocmod.zip ../bin/
+mv breadcrumbs-seo-rdfa.ocmod.zip ../bin/
+
 cd ../bin/
 
-zip -9 -r total-order-discount.zip LICENSE README.md total-order-discount.ocmod.zip
+zip -9 -r breadcrumbs-seo.zip breadcrumbs-seo-microdata.ocmod.zip breadcrumbs-seo-rdfa.ocmod.zip LICENSE README.md
 
 rm -f LICENSE
 rm -f README.md
-rm -f total-order-discount.ocmod.zip
+rm -f breadcrumbs-seo-microdata.ocmod.zip
+rm -f breadcrumbs-seo-rdfa.ocmod.zip
 
 echo DONE.
